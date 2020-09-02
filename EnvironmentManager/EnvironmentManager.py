5f4762279b52b753b65dd39a04b317d585d7f508
0dj0bz <rob@abbottics.ai>
2020-09-02 17:25:46 -0400

    Fixed bug in reading of artifact snippet file - corrected 2 pad bytes that weren't being read corrrectly and caused the data and flag arrays to be misaligned.
    Also began adding EnvironmentManager class.

---
+++ EnvironmentManager/EnvironmentManager.py
@@ -0,0 +1,26 @@
+"""
+EnvironmentManager.py
+"""
+
+import ArtifactSnippet
+
+class EnvironmentManager:
+
+    def __init__(self):
+
+
+    def startEpisode(self):
+
+
+       return episodeId
+
+
+    def getInitialState(self):
+
+
+        return nextState
+
+    def submitAction(self, action):
+
+
+        return isTerminal, nextState, score
