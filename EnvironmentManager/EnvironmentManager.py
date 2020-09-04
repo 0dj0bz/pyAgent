a246d3c61a0e8df325c776e006ad576879ad7c6f
0dj0bz <rob@abbottics.ai>
2020-09-04 17:39:21 -0400

    Added new file pyAgent.py. Also updated EnvironmentManager.py to fix defects.

--- EnvironmentManager/EnvironmentManager.py
+++ EnvironmentManager/EnvironmentManager.py
@@ -1,26 +1,127 @@
 """
 EnvironmentManager.py
 """
+import io
+import struct
+import uuid
 
-import ArtifactSnippet
+class ArtifactSnippet:
+
+# struct EDF_SIGNAL {
+#     char label[16 + 1];
+#     char transducerType[80 + 1];
+#     char physDimension[8 + 1];
+#     char physMinimum[8 + 1];
+#     char physMaximum[8 + 1];
+#     char digiMinimum[8 + 1];
+#     char digiMaximum[8 + 1];
+#     char prefilter[80 + 1];
+#     char numSamples[8 + 1];
+#     char reserved[32 + 1];
+# };
+# struct EEGArtifactV3 {
+#     char version[20] = "EEGArtifactV3";
+#     int channel;
+#     int numSamples;
+#     char label[255];
+#     float sampleStart;
+#     float sampleEnd;
+#     float artStart;
+#     float artEndartEnd;
+#     EDF_SIGNAL signalMetadata;
+# robabbott@robabbott-G7-7790}
+
+    def __init__(self):
+        self.header = None
+        self.version = None
+        self.channel = None
+        self.numRecs = None
+        self.artLabel = None
+        self.sampleStart = None
+        self.sampleEnd = None
+        self.artStart = None
+        self.artEnd = None
+        self.chanLabel = None
+        self.transducerType = None
+        self.physDim = None
+        self.physMin = None
+        self.physMax = None
+        self.digiMin = None
+        self.digiMax = None
+        self.prefilter = None
+        self.numSamples = None
+        self.reserved = None
+        self.data = None
+        self.flags = None
+        self.idx = 0
+
+    def loadfile(self, filename=None):
+        if not filename:
+            return
+
+        with open(filename, "rb") as f:
+
+
+            rawHeader = f.read(568)
+            self.version, self.channel, self.numRecs, self.artLabel, self.sampleStart, self.sampleEnd, \
+            self.artStart, self.artEnd, self.chanLabel, self.transducerType, self.physDim,\
+            self.physMin, self.physMax, self.digiMin, self.digiMax, self.prefilter, self.numSamples,\
+            self.reserved = struct.unpack('20sii255sffff17s81s9s9s9s9s9s81s9s33sxx', rawHeader)
+
+            # now there are two more things to read: an array of shorts of len numRecs an::d
+            # an array of bools of len numRecs.
+
+            rawData = f.read(self.numRecs*2)
+            rawFlags = f.read(self.numRecs*1)
+
+            self.data = struct.unpack(str(self.numRecs)+'h', rawData)
+            self.flags = struct.unpack(str(self.numRecs)+'?', rawFlags)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.idx == len(self.data):
+            raise StopIteration
+
+        self.idx = self.idx + 1
+        return (self.data[self.idx-1], self.flags[self.idx-1])
+
 
 class EnvironmentManager:
 
-    def __init__(self):
+    envId = None
+    snip = None
+    vals = None
 
+    def __init__(self):
+        self.envId = uuid.uuid4()
 
     def startEpisode(self):
-
-
-       return episodeId
-
+        self.snip = ArtifactSnippet()
+        self.snip.loadfile("/home/robabbott/dev/edfviewer/002_art/00000254_s005_t000_ch009-8.art")
+        self.vals = iter(self.snip)
 
     def getInitialState(self):
-
-
+        nextState = self.snip.__next__()
         return nextState
 
     def submitAction(self, action):
+        isTerminal = None
+        nextState = self.snip.__next__()
+        score = None
+
+        return  nextState
 
 
-        return isTerminal, nextState, score
+if (__name__ == "__main__"):
+    em = EnvironmentManager()
+
+    em.startEpisode()
+
+    initState = em.getInitialState()
+
+    print(initState)
+
+    for i in em.snip:
+        print(i)
---
+++ pyAgent.py
@@ -0,0 +1,48 @@
+import EnvironmentManager.EnvironmentManager as envMgr
+import numpy as np
+import os
+import tensorflow as tf
+
+
+
+
+if (__name__ == "__main__"):
+    terminalState = False
+    # configure TF and construct model
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"] = '0'  # Set to -1 if CPU should be used CPU = -1 , GPU = 0
+
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    cpus = tf.config.experimental.list_physical_devices('CPU')
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.LSTM(1, input_shape=[1,1]))
+    model.compile()
+
+    em = envMgr.EnvironmentManager()
+
+    em.startEpisode()
+
+    print ("Episode: ", em.envId)
+
+    curState = em.getInitialState()
+
+    print("Initial State: ", initialState)
+
+    while (not terminalState):
+
+        # predAction = model.predict(x=curState)
+
+        try:
+            # isTerminal, nextState, score = em.submitAction(predAction)
+            # terminalState = isTerminal
+
+            #if score <> 0.0:
+            #    model.fit(x=currentState, y=score)
+
+            curState = em.submitAction(None)
+            print("Next State: ", curState)
+
+        except StopIteration:
+            print("Out of elements")
+            terminalState = True
